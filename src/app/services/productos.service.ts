ed3ca0f98d69afcf9e1db389384b253ea80d53f8
Hector <hdseba.uch@gmail.com>
2017-09-05 13:40:19 -0300

    agregamos firebase al portafolio

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,9 +9,11 @@ export class ProductosService {
     this.cargarProductos();
   }
   public cargarProductos(){
+    this.cargandoproductos=false;
     this.http.get("https://portafolio-15e59.firebaseio.com/productos_idx.json").subscribe(res=>{
-      console.log(res.json());
       this.cargandoproductos=true;
+      this.productos=res.json();
+      console.log(this.productos);
     });
   }
 }
