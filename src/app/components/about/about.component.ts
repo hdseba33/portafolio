9368da17cc4843754aa4ed82e99ac53e14a7451a
Hector <hdseba.uch@gmail.com>
2017-09-05 12:17:07 -0300

    servicio firebase listo

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {InformacionService} from '../../services/informacion.service';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styles: []
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+ // sobrenosotros2:any=[[]];
+  constructor(public is:InformacionService) {
+    // this.sobrenosotros2=is.sobrenosotros;
+  }
 
   ngOnInit() {
   }
--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -3,10 +3,29 @@ import {Http} from '@angular/http';
 @Injectable()
 export class InformacionService {
  info:any={};
+ equipo:any[]=[];
+ cargado:boolean=false;
+ cargadosobrenosotros:boolean=false;
   constructor(public http:Http) {
+    this.cargaDatos();
+    this.cargaSobreNosotros();
+  }
+
+  public cargaDatos(){
     this.http.get("assets/data/info.pagina.json").subscribe(
       data=>{
+        this.cargado=true;
         this.info=data.json();
+      }
+    );
+  }
+
+  public cargaSobreNosotros(){
+    this.http.get("https://portafolio-15e59.firebaseio.com/equipo.json").subscribe(
+      data=>{
+        this.cargadosobrenosotros=true;
+        this.equipo=data.json();
+        console.log(this.equipo);
       }
     );
   }
