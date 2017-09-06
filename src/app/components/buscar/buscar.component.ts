904d9a63acdbd5df4f82f0d39dcff3b069a54b02
Hector <hdseba.uch@gmail.com>
2017-09-06 11:26:47 -0300

    Primera version

--- src/app/components/buscar/buscar.component.ts
+++ src/app/components/buscar/buscar.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute} from '@angular/router';
+import {ProductosService} from '../../services/productos.service';
 @Component({
   selector: 'app-buscar',
   templateUrl: './buscar.component.html',
   styleUrls: ['./buscar.component.css']
 })
 export class BuscarComponent implements OnInit {
+ termino:string;
+ productofiltrado:any[]=[];
+  constructor(private route:ActivatedRoute,private ps:ProductosService) {
+    this.buscarProducto();
+  }
 
-  constructor() { }
+public buscarProducto(){
+  this.route.params.subscribe(parametros=>{
+    this.termino=parametros['termino'];
+  this.ps.buscarProducto(this.termino);
+  });
+}
 
   ngOnInit() {
   }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,23 +1,46 @@
 import { Injectable } from '@angular/core';
 import {Http} from '@angular/http';
-
 @Injectable()
 export class ProductosService {
   productos:any[]=[];
+  productosfiltrado:any[]=[];
   cargandoproductos:boolean=false;
-  producto:any=undefined;
   constructor(private http:Http) {
     this.cargarProductos();
   }
+  public buscarProducto(termino:string){
+    if(this.productos.length===0){
+      this.cargarProductos().then(()=>{
+        this.filtrarProducto(termino);
+      });
+    }
+    else{
+      this.filtrarProducto(termino);
+    }
+  }
+  public filtrarProducto(termino:string){
+    termino.toUpperCase();
+    this.productosfiltrado=[];
+    this.productos.forEach(prod=>{
+      if(prod.categoria.indexOf(termino)>=0 || prod.titulo.indexOf(termino)==0){
+        this.productosfiltrado.push(prod);
+      }
+    });
+  }
+
   public cargarProducto(cod:string){
     return this.http.get("https://portafolio-15e59.firebaseio.com/productos/"+cod+".json");
   }
 
   public cargarProductos(){
-    this.cargandoproductos=false;
-    this.http.get("https://portafolio-15e59.firebaseio.com/productos_idx.json").subscribe(res=>{
-      this.cargandoproductos=true;
-      this.productos=res.json();
-    });
+   let promesa=new Promise((resolve,reject)=>{
+     this.cargandoproductos=false;
+     this.http.get("https://portafolio-15e59.firebaseio.com/productos_idx.json").subscribe(res=>{
+       this.cargandoproductos=true;
+       this.productos=res.json();
+       resolve();
+     });
+   });
+   return promesa;
   }
 }
